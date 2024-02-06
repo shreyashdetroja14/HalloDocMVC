c5b0585f4efde939016f5886d74b9473851fa9a4
Shreyash <shreyash.detroja@etatvasoft.com>
2024-02-06 12:41:52 +0530

    design login, forgot pass pages

---
+++ HalloDocMVC/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HalloDocMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult PatientLogin()
+        {
+            return View();
+        }
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
