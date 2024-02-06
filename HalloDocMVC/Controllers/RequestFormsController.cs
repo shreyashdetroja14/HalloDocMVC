787ef8cfb9da61497859d360860aef02eae06afc
Shreyash <shreyash.detroja@etatvasoft.com>
2024-02-06 14:52:59 +0530

    design request form screens

---
+++ HalloDocMVC/Controllers/RequestFormsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HalloDocMVC.Controllers
+{
+    public class RequestFormsController : Controller
+    {
+        public IActionResult SubmitRequest()
+        {
+            return View();
+        }
+        public IActionResult PatientRequest()
+        {
+            return View();
+        }
+        public IActionResult FamilyRequest()
+        {
+            return View();
+        }
+        public IActionResult ConciergeRequest()
+        {
+            return View();
+        }
+        public IActionResult BusinessRequest()
+        {
+            return View();
+        }
+    }
+}
