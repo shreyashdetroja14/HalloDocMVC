178810fc67eec1a5560b89f2350a78f77fd5c65a
Shreyash <shreyash.detroja@etatvasoft.com>
2024-02-19 17:22:07 +0530

    create account and reset password

--- HalloDocMVC/Controllers/LoginController.cs
+++ HalloDocMVC/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using HalloDocMVC.Models;
+﻿using BCrypt.Net;
+using HalloDocMVC.Models;
+using HalloDocMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -25,17 +27,142 @@ namespace HalloDocMVC.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> CheckLogin(AspNetUser aspnetuser)
         {
-            var user = await _context.AspNetUsers.FirstOrDefaultAsync(u => u.Email == aspnetuser.Email && u.PasswordHash == aspnetuser.PasswordHash);
+            /*var user = await _context.AspNetUsers.FirstOrDefaultAsync(u => u.Email == aspnetuser.Email && u.PasswordHash == aspnetuser.PasswordHash);
 
             if (user == null)
             {
-                ViewBag.Email = "INVALID EMAIL OR PASSWORD";
+                ViewBag.Message = "INVALID EMAIL OR PASSWORD";
                 return View("Index");
             }
             else
             {
-                return RedirectToAction("Dashboard", "Patient", new {user.Id});
+                return RedirectToAction("Dashboard", "Patient", new { user.Id });
+            }*/
+
+            var userFetched = await _context.AspNetUsers.FirstOrDefaultAsync(m => m.Email == aspnetuser.Email);
+            if (userFetched != null)
+            {
+                if (BCrypt.Net.BCrypt.Verify(aspnetuser.PasswordHash, userFetched.PasswordHash))
+                {
+                    return RedirectToAction("Dashboard", "Patient", new { id = userFetched.Id });
+                }
             }
+            return View("Index");
+        }
+
+        public IActionResult CreateAccount()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAccount(CreateAccountViewModel Credentials)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Message = "Please Try Again";
+                return View(Credentials);
+            }
+
+            if (Credentials.Password != Credentials.ConfirmPassword)
+            {
+                ViewBag.Message = "Confirmed Password should match the Password";
+                return View(Credentials);
+            }
+
+            var aspnetuserFetched = await _context.AspNetUsers.FirstOrDefaultAsync(m => m.Email == Credentials.Email);
+            if (aspnetuserFetched != null)
+            {
+                ViewBag.Message = "You already have an account with this email!";
+                return View();
+            }
+            var aspnetuserNew = new AspNetUser();
+            aspnetuserNew.Id = Guid.NewGuid().ToString();
+            aspnetuserNew.UserName = Credentials.Email;
+            aspnetuserNew.Email = Credentials.Email;
+            aspnetuserNew.PasswordHash = BCrypt.Net.BCrypt.HashPassword(Credentials.Password);
+            aspnetuserNew.CreatedDate = DateTime.Now;
+
+            _context.AspNetUsers.Add(aspnetuserNew);
+            await _context.SaveChangesAsync();
+
+            var requestClientFetched = await _context.RequestClients.OrderBy(x => x.RequestClientId).LastOrDefaultAsync(m => m.Email == Credentials.Email);
+
+            var userNew = new User();
+            userNew.AspNetUserId = aspnetuserNew.Id;
+            userNew.Email = aspnetuserNew.Email;
+            userNew.FirstName = requestClientFetched?.FirstName ?? "abc";
+            if (requestClientFetched != null)
+            {
+                var requests = _context.Requests.Where(x => x.RequestId == requestClientFetched.RequestId).ToList();
+                userNew.LastName = requestClientFetched?.LastName;
+                userNew.Mobile = requestClientFetched?.PhoneNumber;
+                userNew.Street = requestClientFetched?.Street;
+                userNew.City = requestClientFetched?.City;
+                userNew.State = requestClientFetched?.State;
+                userNew.ZipCode = requestClientFetched?.ZipCode;
+                userNew.StrMonth = requestClientFetched?.StrMonth;
+                userNew.IntDate = requestClientFetched?.IntDate;
+                userNew.IntYear = requestClientFetched?.IntYear;
+                userNew.CreatedBy = "admin";
+                userNew.CreatedDate = DateTime.Now;
+
+                _context.Users.Add(userNew);
+                await _context.SaveChangesAsync();
+
+                foreach (var request in requests)
+                {
+                    request.UserId = userNew.UserId;
+                    _context.Update(request);
+                }
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                ViewBag.Message = "Looks like you are not eligible to create an account!";
+                return View(Credentials);
+            }
+
+            return View("~/Views/Login/Index.cshtml");
+        }
+
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ResetPassword(CreateAccountViewModel Credentials)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Message = "Please Try Again";
+                return View(Credentials);
+            }
+
+            if (Credentials.Password != Credentials.ConfirmPassword)
+            {
+                ViewBag.Message = "Confirmed Password should match the Password";
+                return View(Credentials);
+            }
+
+            var aspnetuserFetched = await _context.AspNetUsers.FirstOrDefaultAsync(m => m.Email == Credentials.Email);
+            if (aspnetuserFetched == null)
+            {
+                ViewBag.Message = "Please enter correct Email";
+                return View(Credentials);
+            }
+            aspnetuserFetched.PasswordHash = BCrypt.Net.BCrypt.HashPassword(Credentials.Password);
+
+            _context.AspNetUsers.Update(aspnetuserFetched);
+            await _context.SaveChangesAsync();
+
+            return View("~/Views/Login/Index.cshtml");
         }
     }
 }
+
+// $2a$10$2M/4FLCkMI6T5m1yEXg5LOFKro/jJfmGjsihcIe7jpzrWQSYF4sEm
+// $2a$10$Jr4XT9Izy0HfbvJ9Z7HXnOzAPC0OxZpiFL9vkQiDpARg6H7KIChiK
