10a5537969e7507ba11199f70ce65343ec56bb22
Shreyash <shreyash.detroja@etatvasoft.com>
2024-02-20 18:43:39 +0530

    threetier changes

--- HalloDocRepository/Repository/LoginRepository.cs
+++ HalloDocRepository/Repository/LoginRepository.cs
@@ -1,4 +1,5 @@
 ﻿using HalloDocEntities.Data;
+using HalloDocEntities.Models;
 using HalloDocEntities.ViewModels;
 using HalloDocRepository.Repository.Interface;
 using Microsoft.EntityFrameworkCore;
@@ -36,21 +37,55 @@ namespace HalloDocRepository.Repository
 
         public async Task<string> CreateAccount(CreateAccountViewModel Credentials)
         {
-            var aspuserFetched = await _aspnetuserRepository.GetAspNetUserByEmail(Credentials.Email);
-            if(aspuserFetched != null)
+            var aspuserFetched = await _context.AspNetUsers.FirstOrDefaultAsync(m => m.Email == Credentials.Email);
+            if (aspuserFetched != null)
             {
                 return "user exists";
             }
-            var aspnetuserNew = _aspnetuserRepository.CreateAspNetUser(Credentials);
-            var lastRequestClient = _requestClientRepository.GetLastRequestClient(Credentials.Email);
-            if(lastRequestClient != null)
-            {
+            var aspnetuserNew = new AspNetUser();
+            aspnetuserNew.Id = Guid.NewGuid().ToString();
+            aspnetuserNew.UserName = Credentials.Email;
+            aspnetuserNew.Email = Credentials.Email;
+            aspnetuserNew.PasswordHash = BCrypt.Net.BCrypt.HashPassword(Credentials.Password);
+            aspnetuserNew.CreatedDate = DateTime.Now;
 
-            }
-            else
-            {
+            _context.AspNetUsers.Add(aspnetuserNew);
 
+            var requestClientFetched = await _context.RequestClients.OrderBy(x => x.RequestClientId).LastOrDefaultAsync(m => m.Email == Credentials.Email);
+
+            var userNew = new User();
+            userNew.AspNetUserId = aspnetuserNew.Id;
+            userNew.Email = aspnetuserNew.Email;
+            
+
+            if (requestClientFetched != null)
+            {
+                var requests = _context.Requests.Where(x => x.RequestId == requestClientFetched.RequestId).ToList();
+                userNew.FirstName = requestClientFetched.FirstName;
+                userNew.LastName = requestClientFetched?.LastName;
+                userNew.Mobile = requestClientFetched?.PhoneNumber;
+                userNew.Street = requestClientFetched?.Street;
+                userNew.City = requestClientFetched?.City;
+                userNew.State = requestClientFetched?.State;
+                userNew.ZipCode = requestClientFetched?.ZipCode;
+                userNew.StrMonth = requestClientFetched?.StrMonth;
+                userNew.IntDate = requestClientFetched?.IntDate;
+                userNew.IntYear = requestClientFetched?.IntYear;
+                userNew.CreatedBy = "admin";
+                userNew.CreatedDate = DateTime.Now;
+
+                _context.Users.Add(userNew);
+
+
+                foreach (var request in requests)
+                {
+                    request.UserId = userNew.UserId;
+                    _context.Update(request);
+                }
             }
+            
+            await _context.SaveChangesAsync();
+
             return "account created";
         }
     }
