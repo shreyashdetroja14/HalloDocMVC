70217f0669dbfad1b2972341c128c8f1c743c09d
Shreyash Detroja <shrey.detroja14@gmail.com>
2024-04-18 23:12:51 +0530

    global exception handler implemented

---
+++ HalloDocMVC/Controllers/ErrorController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HalloDocMVC.Controllers
+{
+    public class ErrorController : Controller
+    {
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found.";
+                    ViewBag.Path = statusCodeResult?.OriginalPath;
+                    ViewBag.QS = statusCodeResult?.OriginalQueryString;
+                    break;
+            }
+
+            _logger.LogWarning($"404 Error occured. Path = {statusCodeResult?.OriginalPath} and QueryString = {statusCodeResult?.OriginalQueryString}");
+
+            return View("NotFound");
+        }
+
+        [Route("Error")]
+        public IActionResult Error()
+        {
+            var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            ViewBag.ExceptionPath = exceptionDetails?.Path;
+            ViewBag.ExceptionDetails = exceptionDetails?.Error.Message;
+            ViewBag.StackTrace = exceptionDetails?.Error.StackTrace;
+
+            _logger.LogError($"The path {exceptionDetails?.Path} threw an exception {exceptionDetails?.Error}");
+
+            return View("Error");
+        }
+    }
+}
--- HalloDocMVC/Program.cs
+++ HalloDocMVC/Program.cs
@@ -46,9 +46,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    app.UseStatusCodePagesWithReExecute("/Error/{0}");
 }
 
 app.UseHttpsRedirection();
