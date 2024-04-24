e5c2fba76492d2812adcf98412e5d644fd3aa2ae
Shreyash <shreyash.detroja@etatvasoft.com>
2024-04-24 12:55:00 +0530

    assignment project created

---
+++ Assignment/Assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
